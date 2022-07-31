908210ab9a592c16cb5adce826500b4de03d8827
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2022-07-31 14:37:40 +0200

    Second commit

--- src/main/java/com/eara/d/Windows98Machine.java
+++ src/main/java/com/eara/d/Windows98Machine.java
@@ -1,9 +1,10 @@
 package com.eara.d;
 
 /**
- * The principle of dependency inversion refers to the decoupling of software modules.
- * This way, instead of high-level modules depending on low-level modules,
- * both will depend on abstractions.
+ * The principle states that we must use abstraction (abstract classes and interfaces)
+ * instead of concrete implementations. High-level modules should not depend on the low-level module
+ * but both should depend on the abstraction. Because the abstraction does not depend on detail but
+ * the detail depends on abstraction. It decouples the software.
  *
  * Here, we're using the dependency injection pattern to facilitate adding the
  * Keyboard and Monitor dependencies into the Windows98Machine class.
